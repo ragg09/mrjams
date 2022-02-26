387220aeb19353a0cbc6aaae33fddd0be50d1ed5
ragg09 <ragunayon@gmail.com>
2022-02-26 11:45:21 +0800

    feb26 11:43am

--- resources/views/publicViews/index.blade.php
+++ resources/views/publicViews/index.blade.php
@@ -21,7 +21,7 @@
 
   <!-- Forked from a template on Tutorialzine: https://tutorialzine.com/2016/06/freebie-landing-page-template-with-flexbox -->
   <header>
-    <h2><img src="{{ URL::asset('images/mrjams/logowithname.png') }}" class="logotitle"></h2>
+    <h2><img src="{{ URL::asset('images/mrjams/logowithname.PNG') }}" class="logotitle"></h2>
 
     <nav>
       <li><a href="/" style="text-decoration: none; color:black;">Home</a></li>
