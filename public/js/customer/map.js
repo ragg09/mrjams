ad6e1be03222131aa56b4d2083eb4745b4b0749b
ragg09 <ragunayon@gmail.com>
2022-03-28 22:38:46 +0800

    Mar28-10pm

--- public/js/customer/map.js
+++ public/js/customer/map.js
@@ -30,7 +30,7 @@ function initMap() {
                 });
   
                 var marker = new google.maps.Marker({
-                    position: { lat: val.latitude, lng: val.longitude },
+                    position: { lat: parseFloat(val.latitude), lng: parseFloat(val.longitude) },
                     map:map,
                     icon: {
                         url: "/images/mrjams/mr-jams-logo.png",
@@ -124,7 +124,7 @@ function initMap() {
                     });
         
                     var marker = new google.maps.Marker({
-                        position: { lat: val.latitude, lng: val.longitude },
+                        position: { lat: parseFloat(val.latitude), lng: parseFloat(val.longitude) },
                         map:map,
                         icon: {
                             url: "/images/mrjams/mr-jams-logo.png",
