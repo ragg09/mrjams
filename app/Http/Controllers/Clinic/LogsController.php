ec5d0c12296b5eba64ad859cf41cda262f60b7b7
ragg09 <ragunayon@gmail.com>
2022-04-18 13:11:02 +0800

    Apr18-11AM

--- app/Http/Controllers/Clinic/LogsController.php
+++ app/Http/Controllers/Clinic/LogsController.php
@@ -30,7 +30,7 @@ class LogsController extends Controller
             ->where('remark', '!=',  "notif")
             ->where('remark', '!=',  "done_notif")
             ->orderBy('id', 'desc')
-            ->limit(10)
+            ->take(10)
             ->get();
         return view('clinicViews.logs.index', ['data' => $data, 'logs' => $logs]);
     }
@@ -111,7 +111,7 @@ class LogsController extends Controller
 
             $notif = Logs::where('user_as_clinic_id', '=',  $clinic->id)
                 ->whereIn('remark', ["notif", "done_notif"])
-                ->limit(10)
+                ->limit(15)
                 ->orderBy('id', 'desc')
                 ->get();
 
