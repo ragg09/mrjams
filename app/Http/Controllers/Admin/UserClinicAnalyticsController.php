13707588dfd94e04060bb697949ecb35bb38e43a
ragg09 <ragunayon@gmail.com>
2022-04-29 01:38:39 +0800

    Apr29-1AM

--- app/Http/Controllers/Admin/UserClinicAnalyticsController.php
+++ app/Http/Controllers/Admin/UserClinicAnalyticsController.php
@@ -60,7 +60,7 @@ class UserClinicAnalyticsController extends Controller
         //     ->orderBy('created_at', 'asc')
         //     ->get();
 
-        $data = User::select(FacadesDB::raw('count(id) as total'), FacadesDB::raw('MONTH(created_at) as month'))
+        $data = User::select(FacadesDB::raw('count(id) as total'), FacadesDB::raw('EXTRACT(MONTH FROM TIMESTAMP created_at) as month'))
             ->groupby('month')
             ->orderBy('created_at', 'asc')
             ->get();
