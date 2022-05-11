a52a9d01bf01afd4f8c10ad425c6a44c6b5dc535
ragg09 <ragunayon@gmail.com>
2022-05-11 14:24:55 +0800

    May8-11PM

--- resources/views/clinicViews/layouts/master.blade.php
+++ resources/views/clinicViews/layouts/master.blade.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    {{-- <meta http-equiv="X-UA-Compatible" content="ie=edge"> --}}
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> 
     <title>@yield('title')</title>
     
     <link rel="icon" href="{{ URL::asset('images/mrjams/mr-jams-logo.png') }}"/>
