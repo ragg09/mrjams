25f5955d2a4ef9fd650ae73da3477cb8a06c1dba
ragg09 <ragunayon@gmail.com>
2022-05-16 12:44:47 +0800

    May8-11PM

--- app/Http/Controllers/Clinic/LogsController.php
+++ app/Http/Controllers/Clinic/LogsController.php
@@ -141,7 +141,9 @@ class LogsController extends Controller
                         $equipment->quantity = $quant_count;
                         $equipment->save();
 
-                        if ($k->notify == "done") {
+                        $check_logs_exist = Logs::where('message', '=',  "Your stock of " . $key->name . " with expiration date of " . date('M d, Y', strtotime($k->expiration)) . " has expired.")->first();
+
+                        if (!isset($check_logs_exist)) {
                             //checking logs limit 5000
                             if ($logs_count == 5000) {
                                 Logs::where('user_as_clinic_id', '=',  $clinic->id)->first()->delete();
